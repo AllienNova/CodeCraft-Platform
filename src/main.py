b3ea2dca8a49dced22c5ff0f2908d142e7498f13
kimhons <28006680+kimhons@users.noreply.github.com>
2025-09-17 20:06:02 -0400

    Version 46: Save current changes

--- src/main.py
+++ src/main.py
@@ -235,7 +235,7 @@ def dashboard():
 
 @app.route('/learning/magic-workshop')
 def magic_workshop():
-    return render_template('learning/magic_workshop_real_ai.html')
+    return render_template('learning/magic_workshop_enhanced.html')
 
 # Socket.IO events for Professor Sparkle
 @socketio.on('init_sparkle')
